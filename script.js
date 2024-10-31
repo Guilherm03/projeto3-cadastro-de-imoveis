7f5e8235d74dd45e3f3ef9ff5f9db716e5c0136b
João Guilherme Lins <162372169+Guilherm03@users.noreply.github.com>
2024-10-31 08:11:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
+let main;
+const propertyInf = [];
 
-let options 
 do {
-    options = prompt(`
-    1 - Imóveis cadastrados.
-    2 - Salvar um novo imóvel.
-    3 - Listar os imóveis salvos.
-    4 - Sair.`)
+    main = prompt(`
+    Imóveis cadastrados: ${propertyInf.length}
+    1 - Novo imóvel.
+    2 - Listar imóveis salvos.
+    3 - Sair.`);
 
-    switch (options) {
-        case '1':
+    switch (main) {
+        case "1":
+            let property = {};
+            property.name = prompt(`Nome do proprietário:`);
+            property.rooms = parseInt(prompt(`Quantidade de quartos:`));
+            property.bathroom = parseInt(prompt(`Quantidade de banheiros:`));
+            property.garage = prompt(`Tem garagem? (S/N):`);
+            if (confirm(`Deseja salvar este imóvel?\nNome: ${property.name}\nQuartos: ${property.rooms}\nBanheiros: ${property.bathroom}\nGaragem: ${property.garage}`)) {
+                propertyInf.push(property);
+                alert("Imóvel salvo com sucesso!");
+            } else {
+                alert("Imóvel não salvo.");
+            }
+            break;
 
-            if (furniture > 0){
-                alert("Não há imóveis cadrastados")
+        case "2":
+            if (propertyInf.length === 0) {
+                alert("Não há imóveis cadastrados.");
+            } else {
+                let propertyList = propertyInf.map((p, index) => `
+            Imóvel ${index + 1}:
+                Proprietário: ${p.name}
+                Quartos: ${p.rooms}
+                Banheiros: ${p.bathroom}
+                Garagem: ${p.garage}`);
+
+                alert(`Informações dos imóveis:\n${propertyList}`);
             }
-            break
-        case '2':
-            saveProperty{
-                name: 
-            }
-            break
-        case '3':
-            alert("Saindo...")
-            break
+            break;
+
+        case "3":
+            alert("Encerrando...");
+            break;
+
         default:
-            alert("Opção inválida. Tente novamente.")
+            alert("Opção inválida.");
     }
-}while(options !== "4")
+} while (main !== "3");
